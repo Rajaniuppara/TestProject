e78d897a5b869df733aed9bfcb02b895e0954442
hp <hp@LAPTOP-KEC43S4S>
2021-04-08 15:08:02 +0530

    version-1.0

---
+++ JUnitFrameworkDemo/src/demo/FindResult.java
@@ -0,0 +1,38 @@
+package demo;
+
+public class FindResult 
+{
+  public int FindMaxNum(int[] arr)
+  {
+    int max =arr[0];
+     
+    for (int i = 1; i < arr.length; i++)   
+        {  
+             
+                if (max < arr[i])   
+                {  
+                    max=arr[i];
+                }  
+            }  
+        
+    return max;
+    
+  }
+
+public static boolean isEvenNum(int n) {
+	boolean isFlag=false;
+    if(n%2==0)
+    {
+      isFlag=true;
+      
+    }
+    else
+    {
+      isFlag=false;
+    }
+    return isFlag;
+  }
+
+
+}
+
